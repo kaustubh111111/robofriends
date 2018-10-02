a0d0a38451629739718ffa6a114bb5a5aaeaf421
Rohan2599 <201651044@iiitvadodara.ac.in>
2018-10-02 21:23:42 +0530

    updated changes

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -5,11 +5,14 @@ const Card =(props) =>
 {
     const{id,name,email}=props;
 	return(
+
 		<div className='bg-light-green dib br3 pa3 ma2 grow'>
         
 		<img alt='Robots' src={ `https://robohash.org/${id}?200*200` } />
-		<div>
 
+
+		<div>
+		<h1>Hello there </h1>
       <h2> {name} </h2>
       <p> {email} </p> 
 
